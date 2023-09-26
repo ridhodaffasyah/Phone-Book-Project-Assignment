35cb5b6600b048d2c6cc5c345202f2f7042989f2
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-09-26 10:14:47 +0700

    feat: make template for redux persist store and reducers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import { Provider } from 'react-redux';
+import { wrapper } from '@/redux/store/store';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+function App({ Component, ...rest}: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props;
+  const persistor = persistStore(store);
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={
+        <div>Loading...</div>
+      }>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default wrapper.withRedux(App);
---
+++ redux/slice/contactSlice.tsx
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const contactSlice = createSlice({
+    name: 'contact',
+    initialState: {
+        contact: {
+        name: '',
+        email: '',
+        message: '',
+        },
+    },
+    reducers: {
+        setContact: (state, action) => {
+        state.contact = action.payload;
+        },
+    },
+});
+    
+export const { setContact } = contactSlice.actions;
+const contactReducer = contactSlice.reducer;
+export default contactReducer;
