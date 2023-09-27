783f65825c7aa7635d8d57cb6a222c20b3405d68
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-09-28 05:33:29 +0700

    feat: remove redux and replace with apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-import { Provider } from 'react-redux';
-import { wrapper } from '@/redux/store/store';
 import { Loading, Dot, DotContainer } from './styles';
 
-const App = ({ Component, ...rest }: AppProps) => {
-  const { store, props } = wrapper.useWrappedStore(rest);
-  const { pageProps } = props;
-  const persistor = persistStore(store);
+const App = ({ Component, pageProps}: AppProps) => {
 
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor} loading={
-        <Loading>
-          <DotContainer>
-            <h1>Loading</h1>
-            <Dot />
-            <Dot />
-            <Dot />
-          </DotContainer>
-        </Loading>
-      }>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Component {...pageProps} />
+    </>
   );
 };
 
-export default wrapper.withRedux(App);
+export default App;
